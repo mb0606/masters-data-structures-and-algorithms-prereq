c127fc0987a7de7df32e1355ecb2f3b9160df9d2
MKS-students <mks.students@hackreactor.com>
2016-06-01 20:18:17 -0700

    Functional-sahred: queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,29 @@
-var Queue = function(){
+var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.head = 0;
+  queue.tail = 0;
+
+  _.extend(queue,queueMethods);
+  return queue;
 };
 
-var queueMethods = {};
-
-
-
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.tail] = value;
+    this.tail++
+  },
+  dequeue : function(){
+    var result = this.storage[this.head];
+    if(this.size()){
+      delete this.storage[this.head];
+      this.head++;
+    }
+    return result;
+  },
+  size : function(){
+    return this.tail - this.head;
+  }
+};
