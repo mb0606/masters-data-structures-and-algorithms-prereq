aef3368d11167b77211a09c34eadf745abb48e84
MKS-students <mks.students@hackreactor.com>
2016-06-02 21:19:52 -0700

    Hash Tables and Set Complete and all test passing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,15 +4,82 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  // this generates the index for the given key
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(i) ;
+
+  if(!bucket){
+    bucket = [];
+    this._storage.set(i, bucket);
+  };
+
+  var alreadyExists = false;
+
+  for(var pos = 0;  pos < bucket.length;  pos++){
+    var positionInBucket = bucket[pos];
+
+    if( k === positionInBucket[0]){
+      positionInBucket[1] = v;
+      alreadyExists = true;
+    }
+  }
+
+  if(!alreadyExists){
+    bucket.push([k,v]);
+  }
+
+
+
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i)
+
+  var doesExist = false;
+  if(!bucket){
+    return null;
+  }
+  for(var pos = 0; pos < bucket.length; pos++){
+    var positionInBucket = bucket[pos]
+    if( k === positionInBucket[0]){
+      doesExist = true;
+      return positionInBucket[1];
+
+
+    }
+  }
+  if(!doesExist){
+    return null;
+  }
 
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i)
+
+  if(!bucket){
+    return null;
+  }
+   for(var pos = 0; pos < bucket.length; pos++){
+    var positionInBucket = bucket[pos]
+    if( k === positionInBucket[0]){
+      console.log("This is inside the IF",positionInBucket)
+      // bucket.splice(pos, 1);
+            console.log(bucket);
+
+      bucket.splice(pos, 1);
+
+      console.log(bucket);
+
+
+
+    }
+
+  }
 
 };
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item
 };
 
 setPrototype.contains = function(item){
+  var result = false;
+  if(this._storage[item] !== undefined){
+    result = true;
+  }
+  return result;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
