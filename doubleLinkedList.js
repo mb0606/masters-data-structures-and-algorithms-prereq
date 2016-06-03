063aaa32366e4ce7cb77bdc708335c43ca7d38c0
MKS-students <mks.students@hackreactor.com>
2016-06-03 15:31:57 -0700

    Finished Binary tree all test passing, extrat credit double linklist started

---
+++ doubleLinkedList.js
@@ -0,0 +1,86 @@
+var DoubleLinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value){
+    // check if head exists or is null
+    if(this.head === null){
+      //if it doesn't we addd the node to the head;
+      this.head = Node(value);
+      // set tail equal to head since there is only one item
+      this.tail = this.head;
+    } else {
+      // set value of next on current tail to the new NODE
+      this.tail.next = Node(value);
+      //this.tail.next is new node. Need to set previous prop
+      this.tail.next.previous = this.tail;
+      // set tail to the new NODE we created in next to be the tail
+      this.tail = this.tail.next;
+    }
+
+  };
+
+  list.removeFromTail = function(){
+    var tail = this.tail.value;
+    if(this.tail === null){
+      throw error("you dont have a list");
+    } else if (this.tail.previous === null){
+      this.tail = null;
+      this.head = null
+    } else {
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+    }
+    return tail;
+  };
+
+  list.addToHead = function(value){
+    // find current head
+    this.head.previous = Node.value
+
+
+
+  };
+
+  list.removeHead = function(){
+    // set current next of current head
+    var head = this.head.value;
+    if(this.head === null){
+      throw error("You don't have a list")
+    } else if (this.head.next === null){
+      this.head = null;
+      this.tail = null;
+    } else {
+
+      this.head = this.head.next;
+      this.head.previous = null;
+    }
+    return head;
+
+  };
+
+  list.contains = function(target){
+    var result = false;
+    if(this.head.value === target){
+      result = true;
+    }
+    for(var node = this.head.next ; node ; node = node.next){
+      if(node.value === target) result = true;
+    }
+
+    return result;
+  };
+
+  return list;
+};
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+
+  return node;
+};
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,86 @@
 var BinarySearchTree = function(value){
+
+  var searchTree = {};
+
+  searchTree.left = null; // always left than
+  searchTree.right = null;
+  searchTree.value = value;
+
+  _.extend(searchTree, searchTreeMethods);
+
+  return searchTree;
+
 };
 
+
+var searchTreeMethods = {
+  insert: function(value) {
+      // checking if value of child is less than or greater than parent
+      //push(BinarySearchTree(value))
+
+      var traverse = function(tree){
+        if(tree.value > value){
+          if(!tree.left){
+            tree.left = BinarySearchTree(value);
+          } else {
+            traverse(tree.left);
+          }
+        }
+
+        if(tree.value < value){
+          if(!tree.right){
+            tree.right = BinarySearchTree(value);
+          } else {
+            traverse(tree.right);
+          }
+        }
+      }
+
+      traverse(this);
+      console.log(this);
+
+  },
+  contains: function(target) {
+
+    var result = false;
+
+    var traverse = function(tree){
+      if(tree.value === target){
+        result = true;
+      }
+      if(tree.value > target && tree.left){
+        traverse(tree.left);
+      }
+      if(tree.value < target && tree.right){
+        traverse(tree.right);
+      }
+
+    }
+
+    traverse(this);
+    return result;
+
+
+
+  },
+  depthFirstLog: function(callback){
+
+    var traverse = function(tree){
+      if(tree){
+        console.log('callback', tree.value);
+
+
+        callback(tree.value);
+        traverse(tree.left);
+        traverse(tree.right);
+
+      }
+
+
+    }
+    traverse(this);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
