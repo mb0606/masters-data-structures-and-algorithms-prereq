439f29f99e2d256d95cb7af0968eeb90744ce0d2
MKS-students <mks.students@hackreactor.com>
2016-06-04 14:35:38 -0700

    Added Tree remove rebalancing the tree

--- extraCredit/treeExtars.js
+++ extraCredit/treeExtars.js
@@ -41,22 +41,49 @@ treeMethods.contains = function(target){
   return result;
 };
 treeMethods.removeFromParent = function(value){
-    var nodeToRemove;
 
-    var traverse = function(node){
+  var currentNode = this.getNode(value);
+  var parentNode = this.getParentNode(currentNode);
+
+   for( var i = 0; i < currentNode.length; i++){
+     currentNode.children[i].parent = parentNode;
+     parentNode.children.push(currentNode.children[i]);
+   }
+   delete currentNode;
+
+
+}
+
+treeMethods.getNode = function(value){
+  var currentNode;
+  var traverse = function(node){
        if(node.value === value){
-         nodeToRemove = node;
+         currentNode = node;
        }
        for(var i = 0; i < node.children.length; i++){
-
+            traverse(node.children[i]);
        }
-
-
 
     };
     traverse(this);
+    return currentNode;
+};
 
-}
+treeMethods.getParentNode = function(currentNode){
+    var parentNode;
+    var traverse = function(node){
+       if(currentNode.parent === node){
+           parentNode = node;
+       }
+       for(var i = 0; i < node.children.length; i++){
+            traverse(node.children[i]);
+       }
+
+    };
+    traverse(this);
+    return parentNode;
+
+};
 
 
 
